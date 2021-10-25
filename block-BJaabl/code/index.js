9e4b8b2cce393bf57718ae78eba1ffe9ec1b5bf9
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-10-25 21:30:03 +0200

    Update index.js

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,7 +1,9 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-
+function sayHello(name){
+alert("Hello ${name})
+}
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
